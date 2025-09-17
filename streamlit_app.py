00d2adc26d0e8773db092de56b27a086809759b1
Abdur-Rasheed Adeoye <60477717+Abdur-RasheedAde@users.noreply.github.com>
2025-09-17 20:40:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Welcome to my new app; This is Abdur-Rasheed | Bioinformatics and Data Scientist. Welcome! Welcome!! Welcome!!! My new app")
+st.title("🎈 Hey!! Wecome here; This is Abdur-Rasheed | Bioinformatics and Data Scientist.")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
